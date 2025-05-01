3e8b8d4cfdb99365458beb43c5a42c26d6e721ce
ivankazadaev <ivankazadaev21@gmail.com>
2025-05-01 15:21:13 +0200

    Deploy

--- server.cjs
+++ server.cjs
@@ -1,15 +1,18 @@
-const express = require('express');
-const path = require('path');
-
+// server.js
+const express = require("express");
+const path = require("path");
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.resolve(__dirname, 'dist')));
+// Serve static files from dist
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+// Always return index.html for SPA routing
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
